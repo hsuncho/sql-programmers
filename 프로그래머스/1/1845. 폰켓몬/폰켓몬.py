81bc16b5ee674c178a0414a86f6777d77681e999
Hyeonseon Cho <142369881+hsuncho@users.noreply.github.com>
2026-06-02 17:32:32 +0900

    [level 1] Title: 폰켓몬, Time: 0.68 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,2 @@
 def solution(nums):
-    
-    s = set(nums)
-    n = len(nums)
-    answer = 0
-    
-    if int(n/2) > len(s):
-        answer = len(s)
-    else:
-        answer = int(n/2)
-    
-    return answer
+    return min(len(nums)/2, len(set(nums)))
