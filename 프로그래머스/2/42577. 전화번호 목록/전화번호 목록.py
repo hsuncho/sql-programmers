c11f9def151c848d52f519d8717d49204b98e81f
hsuncho <hsuncho@naver.com>
2026-06-06 15:24:04 +0900

    chore: separate algorithm and sql repo

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++
@@ -1,8 +0,0 @@
-def solution(phone_book):
-    s = set(phone_book)
-    for phone in s:
-        for i in range(len(phone)):
-            prefix = phone[:i]
-            if prefix in s:
-                return False
-    return True
